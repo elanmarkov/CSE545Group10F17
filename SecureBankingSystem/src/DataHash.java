cbbea5c8cd6e839d97c21efc61190ddf20f9294c
emarkovcse360 <emarkov@asu.edu>
2017-10-04 13:41:04 -0700

    Implemented SHA-512 data hash and hash salt generator.

--- SecureBankingSystem/src/DataHash.java
+++ SecureBankingSystem/src/DataHash.java
@@ -1,8 +1,19 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
+/*
+ * Secure Data Hashing Algorithm by Elan Markov
+ * CSE 545 Group 10
+ * This function will create a salted hash of the given input.
+ * Also generates salts on request.
+ * Salt stored together with password hash in database. */
 
 public class DataHash {
-	static MessageDigest hasher;
+	MessageDigest hasher;
+	Random saltGen;
+	final int saltSize = 32; // using 32 bit salt
 	public DataHash() {
 		try {
 			hasher = MessageDigest.getInstance("SHA-512");
@@ -10,14 +21,34 @@ public class DataHash {
 			System.err.println("Error: No such algorithm in Java MessageDigest");
 			e.printStackTrace();
 		}
+		saltGen = new SecureRandom();
 	}
-	public static String hash(String password, byte[] salt) {
+	public String hash(String password, String salt) {
 		// hash the message with appended salt and return the hash value
-		String hashVal = "";
-		return hashVal;
+		String hashVal = password + salt;
+		StringBuffer hashBuffer = new StringBuffer();
+		byte[] byteHashVal = hashVal.getBytes();
+		hasher.update(byteHashVal);
+		byte[] byteHash = hasher.digest(); // receive hash value
+		for (byte bytes : byteHash) {
+			//convert hash to hex value
+			hashBuffer.append(String.format("%02x", bytes & 0xff));
+		}
+		hasher.reset();
+		return hashBuffer.toString();
 	}
-	public byte[] getNewSalt() {
-		// Make a new salt to be stored for the user.
-		return null;
+	public String getNewSalt() {
+		// Generate a (secure) random salt and return as a string.
+	    byte[] salt = new byte[saltSize];
+	    saltGen.nextBytes(salt);
+		return new String(salt);
 	}
 }
+
+/*
+ * This will be used for unit tests later
+DataHash hashMachine = new DataHash();
+String salt = hashMachine.getNewSalt();
+System.out.println("New salt: " + salt);
+System.out.println(hashMachine.hash("I love america too much to leave it", salt));
+System.out.println(hashMachine.hash("I don't love france", salt));*/
