7ad5ee4c24733b77760c68b9ebe9851fb4229c96
harsha271094 <harsha271094@gmail.com>
2017-11-02 12:45:41 -0700

    updated dbScripts

--- CSE545Group10F17-kevin3/BankingApp/dbcripts/init_script_deployver.sql
+++ CSE545Group10F17-kevin3/BankingApp/dbcripts/init_script_deployver.sql
@@ -196,3 +196,11 @@ CREATE TABLE completed_internal_requests (
   status varchar(20) NOT NULL, -- Approved or denied
   CONSTRAINT id PRIMARY KEY (id)
 );
+
+
+
+
+INSERT INTO user_login (username, password, enabled, role, accountNonExpired, accountNonLocked, credentialsNonExpired, otpNonLocked) VALUES ("secret@ss.com", "a0a1c1fd5bd5302996afd263581a22cf8aadcea2588018a617f5ab100c7c2c84", 1, "ROLE_ADMIN", 1, 1, 1, 1);
+INSERT INTO users (id, name, role, address, city, state, country, zipcode, phone, email) VALUES (1, 'Administrator', 'ROLE_ADMIN', '123 Administrator Street', 'Temple', 'AZ', 'United States', '85281', '123-456-7890', 'secret@ss.com');
+INSERT INTO user_login_attempts (username, attempts) VALUES ('secret@ss.com', 0);
+INSERT INTO pii_info (id, userId, dob, ssn) VALUES (1, 1, '1-1-1970', '123-45-6789');
